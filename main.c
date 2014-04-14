6c9a8ab95f873b88828ec0b7e118238dc213807e
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-04-14 16:24:31 +0900

    initial commit

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef X
+#error X is not defined
+#endif
+
+void str_to_tale(int *t, char *str)
+{
+        int i;
+
+        for(i=0; i<X*X; i++)
+                t[i]=(int)strtol(str, &str, 10);
+
+        return;
+}
+
+void output_count(unsigned long long int count[X*X][X*X])
+{
+	int i, j;
+
+	fputs("#elem", stdout);
+	for(i=0; i<X*X; i++)
+		printf(" %d", i);
+	putchar('\n');
+	for(i=0; i<X*X; i++){
+		for(j=0; j<X*X; j++)
+			printf(" %llu", count[i][j]);
+		putchar('\n');
+	}
+
+	return;
+}
+
+int main()
+{
+	unsigned long long int count[X*X][X*X];
+	int ms[X*X];
+	int i;
+	char str[0xffff];
+
+	while(!feof(stdin)){
+		fgets(str, 0xffff, stdin);
+		str_to_tale(ms, str);
+		for(i=0; i<X*X; i++)
+			count[i][ms[i]-1]++;
+	}
+
+	output_count(count);
+
+	return 0;
+}
